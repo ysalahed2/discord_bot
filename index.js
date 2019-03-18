54f40599f55c0d6a00451a2790f039474acd9a2e
ysalahed2 <48332386+ysalahed2@users.noreply.github.com>
2019-03-18 21:26:57 +0100

    Update index.js
    Gamedev bot

--- index.js
+++ index.js
@@ -1,21 +1,63 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
-const prefix = '!';
+client.once('ready', () => {
+    console.log('Ready!')
+})
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity(`lastik!`);
-});
-
-bot.on('message', message => {
-	if (message.content.startsWith(`${prefix}QwertyWahtsUrGender`)) {       
-        message.channel.send('tbh i don\'t know but definitely machi rajel :)');
-    }else if (message.content.startsWith(`${prefix}hey`)) {       
-        message.channel.send('Hey smit sidi');
-    }else if (message.content.startsWith(`${prefix}tebi`)) {       
-        message.channel.send('give it to me pls');
+client.on('guildMemberAdd', member => {
+    let role = member.guild.roles.find('name', 'Nomies');
+    let Dev = member.guild.roles.find('name', 'Dev');
+    if (!member.roles.exists(member.displayName,role)) {
+        if(!member.roles.has(Dev.id))
+        member.addRole(role.id);
     }
 });
 
-bot.login(process.env.token);
+
+client.on('message', message => {
+    
+    const dev = message.guild.roles.find('name', 'Dev');
+    const normies = message.guild.roles.find('name', 'Normies');
+    const Mod = message.guild.roles.find('name', 'Mod');
+    const Admin = message.guild.roles.find('name', 'Admin');
+
+    if (message.content.toLowerCase().startsWith("!setdev"))
+    {
+        if(message.member.roles.has(Admin.id) || message.member.roles.has(Mod.id))
+        {
+            if(message.content.toLowerCase() === "!setdev")
+                return message.reply("You didn't specify the user!");
+
+            const user = message.guild.member(message.mentions.users.first());
+
+            if(!user) 
+            {
+                message.reply("Invalid user");
+                return;
+            }
+            else{
+                if(user.roles.has(dev.id))
+                {
+                    message.channel.send(user.displayName + " has the Dev role already!");
+                    return;
+                }
+                else
+                {
+                    if(user.roles.has(normies.id))
+                    {
+                        user.removeRole(normies.id);
+                    }
+                    user.addRole(dev.id);
+                    message.reply("Congrats you are now one of our developpers team!");
+                }
+            }
+        }
+        else
+            return message.channel.send("You don't have the right to use this command!");
+    
+    }
+});
+
+
+client.login(process.env.token);
